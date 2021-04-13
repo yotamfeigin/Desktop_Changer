effbe7a4e29ac31702f22c2238568bc90056a31d
yotamfeigin <Yotam181999@gmail.com>
2021-04-13 17:48:35 +0300

    Update DesktopChanger.py

--- DesktopChanger.py
+++ DesktopChanger.py
@@ -1,9 +1,10 @@
-import fire
+#import fire
 import urllib
 import os
 import requests
 import ctypes
 import logging
+import asyncio
 from time import sleep
 from PIL import Image
 from asyncio import Queue
@@ -19,53 +20,59 @@ leave = 0
 logging.basicConfig(level=logging.INFO)
 
 
-def desktop_changer(changes_num: int = 5):
 
+async def remover(n):
+    await asyncio.sleep(1)
+    for i in range(n):
+        os.remove(storage_path.joinpath(f"CBG{i}.jpg"))
+
+async def setter(waited):
+    global photos_queue
+    print(photos_queue.full())
+    if photos_queue.full():
+        while not photos_queue.empty():
+            logging.info("Current BG is number %s in line",
+                         photos_queue.qsize())
+            (ctypes.windll.user32.SystemParametersInfoW(
+                20, 0, waited, 0))
+            await asyncio.sleep(1.5)
+
+
+def queue():
     global leave
-    photos_queue = Queue(maxsize=changes_num)
-    logging.basicConfig(level=logging.INFO)
-
     logging.info("I will now go fetch %s random pictures then set them as your background", changes_num)
 
-    while leave != "Yes":
+    while not photos_queue.full():
 
         for j in range(changes_num):
-            jason_data = requests.get(API_LINK).json()
-            urllib.request.urlretrieve(
+                jason_data = requests.get(API_LINK).json()
+                urllib.request.urlretrieve(
                 jason_data['urls']['full'], "BackGround")
-            background = Image.open("Background")
-            random_photo = storage_path.joinpath(f'CBG{j}.jpg')
-            background.save(random_photo)
-            photos_queue.put_nowait(random_photo)
-            logging.info(f"Loading background number %s"
-                         f" when i reach %s the magic will begin!",
-                         photos_queue.qsize(), changes_num)
+                background = Image.open("Background")
+                random_photo = storage_path.joinpath(f'CBG{j}.jpg')
+                background.save(random_photo)
+                photos_queue.put_nowait(random_photo)
+                logging.info(f"Loading background number %s"
+                                 f" when i reach %s the magic will begin!",
+                                 photos_queue.qsize(), changes_num)
 
-        sleep(0.5)
-        logging.info("Starting the Desktop change")
-        sleep(1)
+async def main():
 
-        if photos_queue.full():
+    queue()
+    print(photos_queue.full())
+    task1 = asyncio.create_task(setter(str(photos_queue.get_nowait())))
+    task2 = asyncio.create_task(remover(changes_num))
+    await asyncio.gather(
+        task1,task2
 
-            while not photos_queue.empty():
-                logging.info("Current BG is number %s in line",
-                             photos_queue.qsize())
-                ctypes.windll.user32.SystemParametersInfoW(
-                    20, 0, str(photos_queue.get_nowait()), 0)
-                sleep(2)
-
-        logging.info("That's all for now , i shall now delete the backgrounds from your memory")
-        for n in range(changes_num):
-            os.remove(storage_path.joinpath(f"CBG{n}.jpg"))
-
-        leave = input("Would you like to quit ? Type Yes if so , Enter anything else if you wanna go again!")
-
-    else:
-        logging.info("Thanks for using me!")
-        sleep(2)
+    )
 
 
 if __name__ == "__main__":
+    changes_num: int = 5
+    photos_queue = Queue(maxsize=changes_num)
+    logging.basicConfig(level=logging.INFO)
     logging.info("Hello ! Welcome to my background changer.")
-
-    fire.Fire(desktop_changer)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+   # fire.Fire(desktop_changer)
